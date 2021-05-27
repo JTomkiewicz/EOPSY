f325e153b805ff457449fe8565f440518d2d80f1
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-05-27 17:58:08 +0200

    empty func

--- Lab7/philosophers.c
+++ Lab7/philosophers.c
@@ -6,6 +6,24 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <signal.h>
+//threads & mutaxes
+#include <pthread.h>
+
+#define NUM_PHIL 5
+
+//time manipulation
+#define BEGIN_TIME 2
+#define THINK_TIME 2
+#define EAT_TIME 2
+
+// FUNCTIONS //
+void grab_forks(int philo_id)
+{
+}
+
+void put_away_forks(int philo_id)
+{
+}
 
 // MAIN //
 int main()
