8bcf8b2cf76e9a8f7004791c0cbe9ef0a0f3ace1
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-05-27 17:48:57 +0200

    init lab7

---
+++ Lab7/philosophers.c
@@ -0,0 +1,14 @@
+// DINING PHILOSOPHERS PROBLEM IN C WITH THREADS AND MUTAXES //
+// BY JAKUB TOMKIEWICZ //
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+// MAIN //
+int main()
+{
+    return 0;
+}
