571be77ce61e89e67abdf436b2d4e6c603ba8eef
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-05-27 20:33:25 +0200

    pthread beginning

--- Lab7/philosophers.c
+++ Lab7/philosophers.c
@@ -28,5 +28,24 @@ void put_away_forks(int philo_id)
 // MAIN //
 int main()
 {
+    pthread_t philoThreads[NUM_PHIL]; //thread identifiers
+
+    pthread_mutex_t mutex; //mutex locker
+
+    pthread_mutex_t mutexForFork[NUM_PHIL];
+
+    //init mutex lock
+    pthread_mutex_init(&mutex, NULL);
+
+    for (int i = 0; i < NUM_PHIL; i++)
+    {
+        pthread_mutex_init(&mutexForFork[i], NULL); //init mutex lock for mutexes in array
+    }
+
+    //create philosophers
+    for (int i = 0; i < NUM_PHIL; i++)
+    {
+    }
+
     return 0;
 }
