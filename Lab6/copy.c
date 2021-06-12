c2111dd7000452d9c76923b33bbf9f89cb4e21b6
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-06-12 13:10:11 +0200

    open files and close them

--- Lab6/copy.c
+++ Lab6/copy.c
@@ -6,21 +6,43 @@
 #include <stdio.h>
 
 // FUNCTIONS //
-void readWrite() //use read write functions
+void readWrite(int sourceFile, int destinationFile) //use read write functions
 {
 }
 
-void memoryMap() //map files to memory region
+void memoryMap(int sourceFile, int destinationFile) //map files to memory region
 {
+}
+
+void closeFiles(int sourceFile, int destinationFile, int errorID) //close open files
+{
+    switch (errorID)
+    {
+    case 1:
+        break;
+    default:
+        break;
+    }
+
+    //close files
+    if (close(sourceFile) == -1)
+    {
+        printf("Error occured while closing source file. Use -h for help.\n");
+    }
+
+    if (close(destinationFile) == -1)
+    {
+        printf("Error occured while closing source file. Use -h for help.\n");
+    }
 }
 
 // MAIN //
 int main(int argc, char *argv[])
 {
-    int option;
+    int option, useReadWrite = 1;
 
     //process command line options and arguments
-    while ((option = getopt(argc, argv, ":hm:")) != -1) //-1 if there are no more options
+    while ((option = getopt(argc, argv, ":hm")) != -1) //-1 if there are no more options
     {
         if (option == 'h')
         {
@@ -30,6 +52,7 @@ int main(int argc, char *argv[])
         }
         else if (option == 'm')
         {
+            useReadWrite = 0; //when -m use memory map
         }
         else
         {
@@ -37,6 +60,36 @@ int main(int argc, char *argv[])
 
             return 1;
         }
+    }
+
+    //open file to COPY FROM
+    int sourceFile;
+
+    if ((sourceFile = open(argv[optind], O_RDONLY)) == -1)
+    {
+        printf("Error occured while opening source file. Use -h for help.\n");
+
+        return 1;
+    }
+
+    //open file to PASTE TO
+    int destinationFile;
+
+    if ((destinationFile = open(argv[1 + optind], O_CREAT | O_RDWR, S_IRUSR | S_IWUSR)) == -1)
+    {
+        printf("Error occured while opening destiantion file. Use -h for help.\n");
+
+        return 1;
+    }
+
+    //now the copy operation will be performed
+    if (useReadWrite) //readWrite function will be used
+    {
+        readWrite(sourceFile, destinationFile);
+    }
+    else //memoryMap function will be used
+    {
+        memoryMap(sourceFile, destinationFile);
     }
 
     return 0;
