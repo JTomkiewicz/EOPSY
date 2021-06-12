797e808e4bc9a07da425e7f6a06a6efd9f9c5763
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-06-12 12:32:50 +0200

    getopt added

--- Lab6/copy.c
+++ Lab6/copy.c
@@ -3,6 +3,7 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
 // FUNCTIONS //
 void readWrite() //use read write functions
@@ -16,5 +17,27 @@ void memoryMap() //map files to memory region
 // MAIN //
 int main(int argc, char *argv[])
 {
+    int option;
+
+    //process command line options and arguments
+    while ((option = getopt(argc, argv, ":hm:")) != -1) //-1 if there are no more options
+    {
+        if (option == 'h')
+        {
+            printf("Program copies one file to another. Use syntax:\ncopy [-m] <file_name> <new_file_name> \ncopy [-h]\n");
+
+            return 0;
+        }
+        else if (option == 'm')
+        {
+        }
+        else
+        {
+            printf("Wrong parameters. Use -h for help.\n");
+
+            return 1;
+        }
+    }
+
     return 0;
 }
