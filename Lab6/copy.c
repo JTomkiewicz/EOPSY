cb80c49a694c30ad397141403b6fcfe12d561c00
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-06-12 12:05:05 +0200

    init lab6

---
+++ Lab6/copy.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+// FUNCTIONS //
+void readWrite() //use read write functions
+{
+}
+
+void memoryMap() //map files to memory region
+{
+}
+
+// MAIN //
+int main(int argc, char *argv[])
+{
+    return 0;
+}
