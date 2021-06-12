1fc96be8818893242fcf9174beb9be3da942e33a
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-06-12 16:11:58 +0200

    working program

--- Lab6/copy.c
+++ Lab6/copy.c
@@ -4,15 +4,55 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 // FUNCTIONS //
+
+void closeFiles(int sF, int dF, int errorID) //close open files
+{
+    switch (errorID)
+    {
+    case 1:
+        printf("Eror occured in readWrite func during reading.\n");
+        break;
+    case 2:
+        printf("Eror occured in readWrite func during writing.\n");
+        break;
+    case 3:
+        printf("Eror occured in memoryMap func during offset reposition.\n");
+        break;
+    case 4:
+        printf("Eror occured in memoryMap func during mmap sourceFile.\n");
+        break;
+    case 5:
+        printf("Eror occured in memoryMap func during ftruncate.\n");
+        break;
+    case 6:
+        printf("Eror occured in memoryMap func during mmap destinationFile.\n");
+        break;
+    default:
+        break;
+    }
+
+    //close files
+    if (close(sF) == -1)
+    {
+        printf("Error occured while closing source file. Use -h for help.\n");
+    }
+
+    if (close(dF) == -1)
+    {
+        printf("Error occured while closing source file. Use -h for help.\n");
+    }
+}
+
 void readWrite(int sF, int dF) //use read write functions
 {
     //copies chars will be placed in buffer
     char buf[1024];
     int count, errorOccured = 0;
 
-    while (true)
+    while (1)
     {
         errorOccured = 0;
 
@@ -31,44 +71,59 @@ void readWrite(int sF, int dF) //use read write functions
         }
 
         if (errorOccured == 0)
+        {
+            closeFiles(sF, dF, 0);
             break;
+        }
     }
 
-    closeFiles(sF, dF, 0); //close files without error
+    return;
 }
 
 void memoryMap(int sF, int dF) //map files to memory region
 {
+    off_t fileOffset;
+
+    if ((fileOffset = lseek(sF, 0, SEEK_END)) == -1)
+    {
+        closeFiles(sF, dF, 3);
+        return;
+    }
+
+    char *sfData = mmap(NULL, fileOffset, PROT_READ, MAP_PRIVATE, sF, 0);
+
+    if (sfData == MAP_FAILED)
+    {
+        closeFiles(sF, dF, 4);
+        return;
+    }
+
+    if (ftruncate(dF, fileOffset) == -1)
+    {
+        closeFiles(sF, dF, 5);
+        return;
+    }
+
+    char *dfData = mmap(NULL, fileOffset, PROT_WRITE | PROT_READ, MAP_SHARED, dF, 0);
+
+    if (dfData == MAP_FAILED)
+    {
+        closeFiles(sF, dF, 6);
+        return;
+    }
+
+    memcpy(dfData, sfData, fileOffset);
+
+    munmap(sfData, fileOffset);
+    munmap(dfData, fileOffset);
+
     closeFiles(sF, dF, 0); //close files without error
-}
 
-void closeFiles(int sF, int dF, int errorID) //close open files
-{
-    switch (errorID)
-    {
-    case 1:
-        printf("Eror occured in readWrite func during reading.\n");
-        break;
-    case 2:
-        printf("Eror occured in readWrite func during writing.\n");
-        break;
-    default:
-        break;
-    }
-
-    //close files
-    if (close(sF) == -1)
-    {
-        printf("Error occured while closing source file. Use -h for help.\n");
-    }
-
-    if (close(dF) == -1)
-    {
-        printf("Error occured while closing source file. Use -h for help.\n");
-    }
+    return;
 }
 
 // MAIN //
+
 int main(int argc, char *argv[])
 {
     int option, useReadWrite = 1;
